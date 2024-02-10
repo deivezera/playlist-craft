d5536d15a1e6fe424aeb7381e512bb6806e2dee1
deivezera <dzabroski@gmail.com>
2024-02-10 18:46:24 -0300

    first commit, removed files from wdd330 to use project skeleton

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,19 +1,16 @@
-// wrapper for querySelector...returns matching element
+
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-// save data to local storage
+
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-// set a listener for both touchend and click
+
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
@@ -50,7 +47,7 @@ export function renderListWithTemplate(
 
 export function renderWithTemplate(template, parentElement, data, callback) {
   parentElement.insertAdjacentHTML("afterbegin", template);
-  //if there is a callback...call it and pass data
+
   if (callback) {
     callback(data);
   }
